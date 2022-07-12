2c0d6a5291e472d662b31d09db4e6c0526c6f609
Alejandro Martin <7107538+alexis900@users.noreply.github.com>
2022-07-12 06:29:36 +0200

    Curso condicionales

---
+++ condiciones.py
@@ -0,0 +1,13 @@
+edad = int(input("Introduce tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+numero = int(input("Escribe un número"))
+if numero > 5:
+    print("Mayor a 5:")
+elif numero == 5:
+    print("Igual a 5:")
+else:
+    print("Menor a 5:")
